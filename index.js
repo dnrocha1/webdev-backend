1c408831b377d279c6db53478f5cc113afc60a79
Daniyel Rocha <daniyel.rocha@ccc.ufcg.edu.br>
2018-09-12 01:40:33 -0300

    Utilizando mongoose, faltando modelos do sistema

--- index.js
+++ index.js
@@ -3,6 +3,44 @@ const app = express();
 const morgan = require('morgan');
 const cache = require('memory-cache');
 
+/**
+ * MONGOOSE TEST
+ */
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mngr', { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'conection error:'));
+db.once('open', () => {
+    console.log('db connection ok')
+})
+
+const kittySchema = new mongoose.Schema({
+    name: String
+});
+
+kittySchema.methods.speak = function () {
+    let greeting = this.name ? "Meow name is " + this.name : "I don't have a name";
+    console.log(greeting + '\n');
+};
+
+let Kitten = mongoose.model('Kitten', kittySchema);
+
+let fluffy = new Kitten({ name: 'fluffy_kitten'});
+fluffy.speak();
+
+fluffy.save((err, fluffy) => {
+    if(err) return console.error(err);
+    console.log("fluffly saved!")
+    fluffy.speak();
+});
+
+Kitten.find((err, kittens) => {
+    if(err) return console.error(err);
+    console.log(kittens);
+});
+
+
 const PORT = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
@@ -31,6 +69,8 @@ app.use('/amigo', amigo);
  */
 app.use('/grupo', grupo);
 
+/* 
+MEMORY-CACHE
 
 //3
 cache.put('houdini', 'disappear', 1500, (key, value) => {
@@ -44,7 +84,8 @@ console.log('Houdini will now ' + cache.get('houdini'));
 //2
 setTimeout(() => {
     console.log('Houdini is ' + cache.get('houdini'));
-}, 500);
+}, 500); 
+*/
 
 app.listen(PORT, () => {
     process.env.PORT ? console.log("in production") : console.log("in development");
