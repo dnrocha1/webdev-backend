cbe6a82dd9bc65baf9a337b30df69448f607150c
Daniyel Rocha <daniyel.rocha@ccc.ufcg.edu.br>
2018-10-03 00:40:19 -0300

    Usando bcrypt p/ encriptação de senhas de usuários

--- server/user/user.model.js
+++ server/user/user.model.js
@@ -1,4 +1,5 @@
 const moongoose = require('mongoose');
+const bcrpyt = require('bcrypt');
 
 const UserSchema = new moongoose.Schema({
     name: {
@@ -20,6 +21,23 @@ const UserSchema = new moongoose.Schema({
     favUsers: [{
         type: moongoose.Schema.Types.ObjectId, ref: 'User'
     }]
+});
+
+UserSchema.pre('save', function(next) {
+    const user = this;
+    if (!user.isModified('password')) return next();
+
+    bcrpyt.genSalt(10, function(err, salt) {
+        if (err) return next(err);
+
+        bcrpyt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            user.password = hash;
+            next();
+        });
+    });
+
 });
 
 module.exports = moongoose.model('User', UserSchema);
