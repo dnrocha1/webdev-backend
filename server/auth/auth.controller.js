ee8b330e2f654a4ef613dd9a55fc4cd57bffa82b
Daniyel Rocha <daniyel.rocha@ccc.ufcg.edu.br>
2018-10-08 21:45:37 -0300

    Refatora auth.js

--- server/auth/auth.controller.js
+++ server/auth/auth.controller.js
@@ -1,9 +1,7 @@
 const jwt = require('jsonwebtoken');
 const userCtrl = require('../user/user.controller');
 
-config = {
-    jwtSecret: 'asdf'//colocar isso em outro lugar
-}
+const config = require('../../config/config');
 
 const login = (req, res, next) => {
 
@@ -12,27 +10,26 @@ const login = (req, res, next) => {
 
     userCtrl.getUserByEmail(userEmail)
         .then((user) => {
-            if(user && userEmail === user.email && user.comparePassword(userPassword, user.password)) {
-                const token = jwt.sign({
-                    _id: user._id,
-                    username: user.username,
-                    email: user.email,
-                    //role: ADMIN
-                },  config.jwtSecret);
 
-                return res.json({userId: user._id, token});
-
-            } else {
-                const err = {'message':'SENHA INCORRETA'};
-                return res.json(err);
-                //return next(err);
+            if(!user) {
+                return res.json({ 'message':'Failed. User not found.' });
+            } else if(user) {
+                if(userEmail === user.email && user.comparePassword(userPassword, user.password)) {
+                    const token = jwt.sign({
+                        _id: user._id,
+                        email: user.email,
+                        //role: ADMIN
+                    },  config.jwtSecret);
+                    return res.json({ userId: user._id, token })
+                } else {
+                    return res.json({ 'message':'Failed. Wrong password.' });
+                }
             }
         })
         .catch((err) => {
-            const error = {'message':'ANY OTHER ERROR'};
+            const error = {' message':'Something went wrong, try again! '};
             return res.json(error);
-            //return next(error);
         });
 }
 
-module.exports = {login}
+module.exports = {login};
